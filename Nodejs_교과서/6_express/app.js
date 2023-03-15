b71f29ec1f4fbb76c1c2588bbc84821a66cb88c7
JongYeonHwang <junyon6@gmail.com>
2023-03-15 19:01:03 +0900

    6.1 express 프로젝트 시작하기 완료 2023-03-15 19:00

---
+++ Nodejs_교과서/6_express/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const express = require('express');
+const path = require('path');
+
+const app= express();
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+    // res.send('hello express');
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트 실행');
+});
