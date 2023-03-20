cf2af2ae7c79a5681d7b05f19b9a8c8ec5345b01
JongYeonHwang <junyon6@gmail.com>
2023-03-20 18:32:52 +0900

    10.1 웹 API 서비스 만들기 시작

--- Nodejs_교과서/9_nodebird/app.js
+++ Nodejs_교과서/9_nodebird/app.js
@@ -9,9 +9,11 @@ const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 const passport = require('passport');
 
-dotenv.config('dev');
+dotenv.config();
 const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
+const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
@@ -34,6 +36,7 @@ sequelize.sync({ force: false })
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -46,11 +49,13 @@ app.use(session({
         secure: false,
     },
 }));
-app.use(passport.initialize());
+app.use(passport.initialize());         // passport는 반드시 express-session 설정 후에 설정
 app.use(passport.session());
 
 app.use('/', pageRouter);
 app.use('/auth', authRouter);
+app.use('/post', postRouter);
+app.use('/user', userRouter);
 
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
--- Nodejs_교과서/9_nodebird/routes/page.js
+++ Nodejs_교과서/9_nodebird/routes/page.js
@@ -2,16 +2,15 @@
 
 const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('../middlewares');
-const { renderProfile, renderJoin, renderMain } = require('../controllers/page');
+const { renderProfile, renderJoin, renderMain, renderHashtag } = require('../controllers/page');
 
 const router = express.Router();
 
 router.use((req, res, next) => {
     res.locals.user = req.user;
-    res.locals.user = null;
-    res.locals.followerCount = 0;
-    res.locals.followingCount = 0;
-    res.locals.followingIdList = [];
+    res.locals.followerCount = req.user?.Followers?.length || 0;
+    res.locals.followingCount = req.user?.Followings?.length || 0;
+    res.locals.followingIdList = req.user?.Followings?.map(f => f.id) || [];
     next();
 });
 
@@ -20,5 +19,7 @@ router.get('/profile', isLoggedIn, renderProfile);
 router.get('/join', isNotLoggedIn, renderJoin);
 
 router.get('/', renderMain);
+
+router.get('/hashtag', renderHashtag);
 
 module.exports = router;
