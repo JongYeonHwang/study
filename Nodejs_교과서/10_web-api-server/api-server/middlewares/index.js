c8dfdefb237a4f1028f8fc233288e5d0d83584ea
JongYeonHwang <junyon6@gmail.com>
2023-03-22 15:25:43 +0900

    7.6 웹 API 서버 만들기 완 2023-03-22 15:25

--- Nodejs_교과서/10_web-api-server/api-server/middlewares/index.js
+++ Nodejs_교과서/10_web-api-server/api-server/middlewares/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const jwt = require('jsonwebtoken');
+const rateLimit = require('express-rate-limit');
 
 exports.isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated) {
@@ -35,4 +36,22 @@ exports.verifyToken = (req, res, next) => {
             message: '유효하지 않은 토큰입니다.',
         });
     }
+};
+
+exports.apiLimiter = rateLimit({
+    windowMs: 60 * 1000,            // 1분
+    max: 1,
+    handler(req, res) {
+        res.status(this.statusCode).json({
+            code: this.statusCode,   // 기본값 429
+            message: '1분에 한번만 요청할 수 있습니다.'
+        });
+    },
+});
+
+exports.deprecated = (req, res) => {
+    res.status(410).json({
+        code: 410,
+        message: '새로운 버전이 나왔습니다. 새로운 버전을 사용하세요',
+    });
 };
